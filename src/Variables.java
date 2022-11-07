97901feb34ba1a780590761b1bceaaba4aaaef52
IVIaike <trainer.ekb@gmail.com>
2022-11-07 19:13:49 +0500

    Задача 1 на присвоение переменных выполнена

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        var dog = 8;
+        System.out.println("dog = "+dog);
+        var cat = 3.6;
+        System.out.println("cat = "+cat);
+        var paper = 763789;
+        System.out.println("paper = "+paper);
+    }
+}
